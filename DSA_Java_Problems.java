ec33eebb94908596803396591817910df227cf05
balaji6167 <balajiparamasivam1107@gmail.com>
2026-02-21 20:50:34 +0530

    first commit

---
+++ DSA_Java_Problems.java
@@ -0,0 +1,46 @@
+//    Leetcode :       260. Single Number III
+// Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+// You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+
+// Example 1:
+
+// Input: nums = [1,2,1,3,2,5]
+// Output: [3,5]
+// Explanation:  [5, 3] is also a valid answer.
+// Example 2:
+
+// Input: nums = [-1,0]
+// Output: [-1,0]
+// Example 3:
+
+// Input: nums = [0,1]
+// Output: [1,0]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 3 * 104
+// -231 <= nums[i] <= 231 - 1
+// Each integer in nums will appear twice, only two integers will appear once.
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DSA_Java_Problems {
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,3,2,5};
+        HashMap<Integer,Integer> Map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            Map.put(nums[i],Map.getOrDefault(nums[i],0)+1);
+        }
+        int[] arr = new int[2];
+        int index = 0;
+        for(Map.Entry<Integer,Integer> entry:Map.entrySet()){
+            if(entry.getValue()==1){
+                arr[index++] = entry.getKey();
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
