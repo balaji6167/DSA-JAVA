56d6f02369fa56fbf02ac19ef658d6ae6a729f4e
balaji6167 <balajiparamasivam1107@gmail.com>
2026-03-02 12:23:44 +0530

    sort colors 0's 1's 2's

---
+++ Sort_colors.java
@@ -0,0 +1,48 @@
+// Sort 0s, 1s and 2s
+
+// Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+// Note: You need to solve this problem without utilizing the built-in sort function.
+
+// Examples:
+
+// Input: arr[] = [0, 1, 2, 0, 1, 2]
+// Output: [0, 0, 1, 1, 2, 2]
+// Explanation: 0s, 1s and 2s are segregated into ascending order.
+// Input: arr[] = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+// Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+// Explanation: 0s, 1s and 2s are segregated into ascending order.
+// Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+// Constraints:
+// 1 ≤ arr.size() ≤ 10^6
+// 0 ≤ arr[i] ≤ 2
+
+// Expected Complexities
+// Time Complexity: O(n)
+// Auxiliary Space: O(1)
+
+public class Sort_colors {
+    public void sort012(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                int temp = arr[high];
+                arr[high] = arr[mid];
+                arr[mid] = temp;
+                high--;
+            }
+        }
+    }
+}
