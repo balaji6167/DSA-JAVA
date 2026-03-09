8e4251fb39242aff2f7d25246bb6651cb729dce2
balaji6167 <balajiparamasivam1107@gmail.com>
2026-03-09 19:10:04 +0530

    Floor in a sorted array

---
+++ Floor_array.java
@@ -0,0 +1,40 @@
+// Floor in a Sorted Array
+
+// Given a sorted array arr[] and an integer x, find the index (0-based) of the largest element in arr[] that is less than or equal to x. This element is called the floor of x. If such an element does not exist, return -1.
+
+// Note: In case of multiple occurrences of floor of x, return the index of the last occurrence.
+
+// Examples
+
+// Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 5
+// Output: 1
+// Explanation: Largest number less than or equal to 5 is 2, whose index is 1.
+// Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 11
+// Output: 4
+// Explanation: Largest Number less than or equal to 11 is 10, whose indices are 3 and 4. The index of last occurrence is 4.
+// Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 0
+// Output: -1
+// Explanation: No element less than or equal to 0 is found. So, output is -1.
+// Constraints:
+// 1 ≤ arr.size() ≤ 106
+// 1 ≤ arr[i] ≤ 10^6
+// 0 ≤ x ≤ arr[n-1]
+
+public class Floor_array {
+    public int findFloor(int[] arr, int x) {
+        int l = 0;
+        int r = arr.length-1;
+        int ans = -1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(arr[mid]<=x){
+                ans = mid;
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+}
