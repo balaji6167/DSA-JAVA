56eb0c7131aa9abd87a7d961b582550a67572a9a
balaji6167 <balajiparamasivam1107@gmail.com>
2026-03-11 19:17:40 +0530

    Complement of Base 10 Integer

---
+++ Complement_of_Base.java
@@ -0,0 +1,59 @@
+// leetcode 1009. Complement of Base 10 Integer
+
+// The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+// For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+// Given an integer n, return its complement.
+
+ 
+
+// Example 1:
+
+// Input: n = 5
+// Output: 2
+// Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
+// Example 2:
+
+// Input: n = 7
+// Output: 0
+// Explanation: 7 is "111" in binary, with complement "000" in binary, which is 0 in base-10.
+// Example 3:
+
+// Input: n = 10
+// Output: 5
+// Explanation: 10 is "1010" in binary, with complement "0101" in binary, which is 5 in base-10.
+ 
+
+// Constraints:
+
+// 0 <= n < 10^9
+
+public class Complement_of_Base {
+    public int bitwiseComplement(int n) {
+
+        // Approach 1:
+        String str = Integer.toBinaryString(n);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='1'){
+                sb.append(0);
+            }
+            else{
+                sb.append(1);
+            }
+        }
+        String binary = sb.toString();
+        int ans = Integer.parseInt(binary,2);
+        return ans;
+
+        // Approach 2:
+        if (n == 0) return 1;
+        int mask = 0;
+        int temp = n;
+        while (temp > 0) {
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+        return n ^ mask;
+    }
+}
