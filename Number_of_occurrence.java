edc974790b31ee6baba966fd9d44df9546ffc48c
balaji6167 <balajiparamasivam1107@gmail.com>
2026-03-24 14:00:49 +0530

    Number of occurrence

---
+++ Number_of_occurrence.java
@@ -0,0 +1,33 @@
+// Number of occurrence
+// Difficulty: EasyAccuracy: 59.34%Submissions: 389K+Points: 2Average Time: 20m
+// Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+
+// Examples :
+
+// Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+// Output: 4
+// Explanation: target = 2 occurs 4 times in the given array so the output is 4.
+// Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 4
+// Output: 0
+// Explanation: target = 4 is not present in the given array so the output is 0.
+// Input: arr[] = [8, 9, 10, 12, 12, 12], target = 12
+// Output: 3
+// Explanation: target = 12 occurs 3 times in the given array so the output is 3.
+// Constraints:
+// 1 ≤ arr.size() ≤ 10^6
+// 1 ≤ arr[i] ≤ 10^6
+// 1 ≤ target ≤ 10^6
+
+
+
+public class Number_of_occurrence{
+    int countFreq(int[] arr, int target) {
+        int freq = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                freq++;
+            }
+        }
+        return freq;
+    }
+}
