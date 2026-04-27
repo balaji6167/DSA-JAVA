0360220c25b8c2f3b803dc975733533fa5a226c6
balaji6167 <balajiparamasivam1107@gmail.com>
2026-04-27 13:54:10 +0530

    Matrix Diagonal Sum

---
+++ DiagonalSum.java
@@ -0,0 +1,51 @@
+// (leetcode) :1572. Matrix Diagonal Sum
+
+// Given a square matrix mat, return the sum of the matrix diagonals.
+// Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+// Example 1:
+
+// Input: mat = [[1,2,3],
+//               [4,5,6],
+//               [7,8,9]]
+// Output: 25
+// Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+// Notice that element mat[1][1] = 5 is counted only once.
+// Example 2:
+
+// Input: mat = [[1,1,1,1],
+//               [1,1,1,1],
+//               [1,1,1,1],
+//               [1,1,1,1]]
+// Output: 8
+// Example 3:
+
+// Input: mat = [[5]]
+// Output: 5
+ 
+
+// Constraints:
+
+// n == mat.length == mat[i].length
+// 1 <= n <= 100
+// 1 <= mat[i][j] <= 100
+
+
+public class DiagonalSum {
+    public int diagonalSum(int[][] mat) {
+        int matrixDiagonalSum = 0;
+        int m = mat.length;
+        int n = mat[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==j){
+                    matrixDiagonalSum += mat[i][j];
+                }
+                else if(i!=j && i+j==m-1){
+                    matrixDiagonalSum += mat[i][j];
+                }
+            }
+        }
+        return matrixDiagonalSum;
+    }
+}
