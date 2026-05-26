b376fe3ece7d58038fb09529e62182fa5969e35f
Balaji P <balajiparamasivam1107@gmail.com>
2026-05-26 19:13:54 +0530

    Update comment for problem statement clarity

--- SpecialCharactersI.java
+++ SpecialCharactersI.java
@@ -1,4 +1,4 @@
-// 3120. Count the Number of Special Characters I:
+// (LeetCode): 3120. Count the Number of Special Characters I:
 // You are given a string word. A letter is called special if it appears both in lowercase and uppercase in word.
 // Return the number of special letters in word.
 
