ac7954ae9d2cbab029a77a08767827c63b1f4670
balaji6167 <balajiparamasivam1107@gmail.com>
2026-06-12 18:19:33 +0530

    Most Common Word

---
+++ MostCommon.java
@@ -0,0 +1,58 @@
+// (LeetCode): 819. Most Common Word:
+
+// Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
+// The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+// Note that words can not contain punctuation symbols.
+
+// Example 1:
+
+// Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+// Output: "ball"
+// Explanation: 
+// "hit" occurs 3 times, but it is a banned word.
+// "ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph. 
+// Note that words in the paragraph are not case sensitive,
+// that punctuation is ignored (even if adjacent to words, such as "ball,"), 
+// and that "hit" isn't the answer even though it occurs more because it is banned.
+
+// Example 2:
+
+// Input: paragraph = "a.", banned = []
+// Output: "a"
+ 
+// Constraints:
+// 1 <= paragraph.length <= 1000
+// paragraph consists of English letters, space ' ', or one of the symbols: "!?',;.".
+// 0 <= banned.length <= 100
+// 1 <= banned[i].length <= 10
+// banned[i] consists of only lowercase English letters.
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+public class MostCommon {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph = paragraph.replaceAll("[^a-zA-Z0-9]"," ").toLowerCase();
+        Set<String> bannedset = new HashSet<>();
+        for(int i=0;i<banned.length;i++){
+            bannedset.add(banned[i].toLowerCase());
+        }
+        Map<String,Integer> map = new HashMap<>();
+        String[] para = paragraph.split(" ");
+        for(String word:para){
+            if(!word.isEmpty() &&!bannedset.contains(word)){
+                map.put(word,map.getOrDefault(word,0)+1);
+            } 
+        }
+        String result = "";
+        int max = 0;
+        for(Map.Entry<String,Integer> entry:map.entrySet()){
+            if(entry.getValue()>max){
+                max = entry.getValue();
+                result = entry.getKey();
+            }
+        }
+        return result;
+    }   
+}
